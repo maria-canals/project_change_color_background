3fa0016b311be257a6e8d998b6d92cd1739bba02
maria-canals <mariacanals23@gmail.com>
2021-01-03 00:00:43 +0100

    Co-authored-by: Oscar Farré Andrés <oscar.farre@student.bit.es>

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 //Choose a random color
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+let arrayOfColors = ["green", "red", "blue", "yellow", "purple", "black", "orange"] 
 
+document.querySelector('.btn').addEventListener('click', changeColor)
 
-/**
- * 1. Necesitamos crear una función que devuelva un color aleatorio entre todos los colores del array 'colors'. Crea esta función o mira por Internet como hacer para escoger una posición aleatorio de un array
- */
-
-function getRandomColor() {
-    // Si bien normalmente las funciones no conocen nada del exterior; estos casos son una buena excepción. La lista de colores se puede considerar una cofiguración "global" a la cual cualqueir función debe poder acceder.
-
-    console.log(colors)
+//get random index
+function randomColor(){
+    let numOfColors = arrayOfColors.length;
+    let randomIndex = Math.floor(Math.random() * numOfColors)
+    return arrayOfColors[randomIndex]
 }
 
-/**
- * 2. Debemos seleccionar el botón a clicar, y todo el elemento 'body'. Guarda cada uno de los elementos en una variable. Deberían ser tan solo dos líneas de código. Muestra por consola el valor de las variables para comprobar que has seleccionado los elementos adecuados
- */
-
-// let body ....
-// let button ...
-
- /**
-  * 3. Asocia al botón 'button' un evento onclick. Dicho evento debe llamar a una función; que se va a encargar de:
-  * 
-  *   A) Llamar a la otra función 'getRandomColor' y obtener un color aleatorio
-  *    B) Modificar el 'backgroundColor' del body con el valor que hemos conseguido. Deves investigar como cambiar el color del fondo de un elemento HTML a través de JavaScript
-  */
+//change Color
+function changeColor(){
+    document.body.style.backgroundColor = randomColor()
+}
